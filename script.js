4f349781b5ad254a751f98fc9777471d7be0d351
TDung939 <dungnguyenhoangtrung@gmail.com>
2020-11-03 14:44:35 +0700

    Fixed template

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-60px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('nav-ul');
 
 hamburger.addEventListener('click', () => {
     navUL.classList.toggle('show');
 })
-
-
